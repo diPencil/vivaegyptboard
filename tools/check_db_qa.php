db10c7e602ce94ea325dbb8abf5748d0f64bbcaf
diPencil <dipencilcom@gmail.com>
2026-07-30 22:42:13 +0300

    chore: remove temporary QA and runtime session files

--- tools/check_db_qa.php
+++
@@ -1,3 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=127.0.0.1;port=3306;dbname=vivaboard_rotation_qa', 'root', '');
-print_r($pdo->query('SHOW COLUMNS FROM employee_shift_schedules')->fetchAll(PDO::FETCH_COLUMN));
